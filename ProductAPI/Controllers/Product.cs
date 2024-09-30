bd8f9763a4dce7531124f42d18bea196ff9c5f1b
PoznanaKing <pozsgain@kkszki.hu>
2024-09-30 10:21:57 +0200

    Connection is made between the program and the database, product class is ready to use, controller is already generated but not set anything in it.

---
+++ ProductAPI/Controllers/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductAPI.Controllers
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
